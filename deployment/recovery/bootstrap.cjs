66961498d4276284c36bf33f4c7b7edd9e1f87e1
Maryam Jejeola <141655026+Jejeola11@users.noreply.github.com>
2026-09-09 01:22:06 +0100

    Deploy latest Fuse source branch during Vercel recovery build

--- deployment/recovery/bootstrap.cjs
+++ deployment/recovery/bootstrap.cjs
@@ -3,7 +3,7 @@ const fs=require('node:fs'),os=require('node:os'),path=require('node:path');
 const tmp=fs.mkdtempSync(path.join(os.tmpdir(),'fuse-recovery-'));
 try {
  execFileSync('git',['init',tmp]);
- execFileSync('git',['-C',tmp,'fetch','--depth','1','https://github.com/Jejeola11/Jejeola11.git','a8d76b5880bc7b6871a7c02507023bca62a917f3'],{stdio:'inherit'});
+ execFileSync('git',['-C',tmp,'fetch','--depth','1','https://github.com/Jejeola11/Jejeola11.git','fix/fuse-app-navigation'],{stdio:'inherit'});
  execFileSync('git',['-C',tmp,'checkout','FETCH_HEAD'],{stdio:'inherit'});
  for(const name of ['app','atelier-v2','atelier-site','api','server','package.json','package-lock.json']) fs.cpSync(path.join(tmp,name),name,{recursive:true});
 } finally {fs.rmSync(tmp,{recursive:true,force:true});}
